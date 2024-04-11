8a9aa247c4cdb4a933007d1d85a98e2d1222023b
tyndev <17089219+tyndev@users.noreply.github.com>
2024-04-11 07:26:09 -0700

    add vector store and query engine

--- starter.py
+++ starter.py
@@ -1,7 +1,14 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
 
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
 
